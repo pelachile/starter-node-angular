2548698e7af0573ebce96ac5b01539fa8344d461
Cory Moore <cory@corymoore.com>
2019-02-18 12:26:01 -0600

    added private key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.94.171.128',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'lesson-five.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
